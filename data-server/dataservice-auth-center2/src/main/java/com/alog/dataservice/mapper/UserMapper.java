c4260445a1e28bfe92c5b47f2972bcfa57f1aa21
huangzhuowei <zhuoweihuang@139.com>
2019-04-23 11:58:05 +0800

    删除多余的demo

--- data-server/dataservice-auth-center2/src/main/java/com/alog/dataservice/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.alog.dataservice.mapper;
-
-import com.alog.dataservice.entity.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserMapper {
-    User findByUsername(@Param("username") String username);
-    User findUserById(@Param("Id") String Id);
-}
